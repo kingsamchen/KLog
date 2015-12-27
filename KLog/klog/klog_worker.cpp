afc3eb9f267253b33c042229389742ccab122d6a
kingsleychen <kingsamchen@gmail.com>
2015-12-27 17:08:23 +0800

    Done main structure of logging worker.

--- KLog/klog/klog_worker.cpp
+++ KLog/klog/klog_worker.cpp
@@ -4,6 +4,66 @@
 
 #include "klog/klog_worker.h"
 
+namespace {
+
+const size_t kDefaultBufferSize = 4 * 1024 * 1024;
+
+}   // namespace
+
 namespace klog {
+
+LogWorker::LogWorker(const PathString& log_file_path, std::chrono::seconds flush_interval)
+    : log_file_path_(log_file_path), flush_interval_(flush_interval), done_(false)
+{
+    working_buffer_.reserve(kDefaultBufferSize);
+    backlog_buffer_.reserve(kDefaultBufferSize);
+    thread_ = std::thread(&LogWorker::WorkFunc, this);
+}
+
+LogWorker::~LogWorker()
+{
+    done_.store(true, std::memory_order::memory_order_release);
+    thread_.join();
+}
+
+
+void LogWorker::Send(std::string&& message)
+{
+    std::lock_guard<std::mutex> lock(mutex_);
+    if (working_buffer_.capacity() - working_buffer_.size() > message.size()) {
+        working_buffer_.insert(working_buffer_.end(), message.begin(), message.end());
+    } else {
+        using std::swap;
+        swap(working_buffer_, backlog_buffer_);
+        working_buffer_.insert(working_buffer_.end(), message.begin(), message.end());
+        cond_.notify_one();
+    }
+}
+
+void LogWorker::WorkFunc()
+{
+    Buffer relay_buffer;
+    relay_buffer.reserve(kDefaultBufferSize);
+    while (!done_.load(std::memory_order::memory_order_acquire)) {
+        {
+            std::unique_lock<std::mutex> lock(mutex_);
+            // May be waked up due to either being signaled or tiemout.
+            cond_.wait_for(lock, flush_interval_);
+            using std::swap;
+            if (backlog_buffer_.empty()) {
+                swap(working_buffer_, relay_buffer);
+            } else {
+                swap(backlog_buffer_, relay_buffer);
+            }
+        }
+
+        if (relay_buffer.empty()) {
+            continue;
+        }
+
+        // TODO: write to disk.
+        relay_buffer.clear();
+    }
+}
 
 }   // namespace klog
--- KLog/klog/klog_worker.h
+++ KLog/klog/klog_worker.h
@@ -1,5 +1,5 @@
 /*
- @ 0xCCCCCCCC
+@ 0xCCCCCCCC
 */
 
 #if defined(_MSC_VER)
@@ -9,8 +9,11 @@
 #ifndef KLOG_KLOG_WORKER_H_
 #define KLOG_KLOG_WORKER_H_
 
+#include <atomic>
 #include <chrono>
+#include <mutex>
 #include <thread>
+#include <vector>
 
 #include "klog/basic_macros.h"
 #include "klog/basic_types.h"
@@ -18,6 +21,32 @@
 namespace klog {
 
 class LogWorker {
+public:
+    LogWorker(const PathString& log_file_path, std::chrono::seconds flush_interval);
+
+    ~LogWorker();
+
+    DISALLOW_COPY(LogWorker);
+
+    DISALLOW_MOVE(LogWorker);
+
+    void Send(std::string&& message);
+
+private:
+    void WorkFunc();
+
+private:
+    using Buffer = std::vector<char>;
+
+private:
+    PathString log_file_path_;
+    std::chrono::seconds flush_interval_;
+    Buffer working_buffer_;
+    Buffer backlog_buffer_;
+    std::atomic<bool> done_;
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    std::thread thread_;
 };
 
 }   // namespace klog
