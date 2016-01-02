cdcba9184c45ff9000d73913c0b34ab7f201187f
kingsleychen <kingsamchen@gmail.com>
2016-01-03 02:23:44 +0800

    Update quick test case.

--- KLog/main.cpp
+++ KLog/main.cpp
@@ -4,16 +4,22 @@
 
 #include <conio.h>
 
+#include <iomanip>
 #include <iostream>
 #include <random>
 #include <sstream>
 
+#include <windows.h>
+
 #include "klog/klog.h"
 #include "klog/klog_worker.h"
 
+namespace chrono = std::chrono;
+
 void TestWorker()
 {
-    klog::LogWorker worker(std::wstring(L"test.log"), std::chrono::seconds(3));
+    klog::LogWorker worker([] { return PATH_LITERAL("test.log"); }, std::chrono::seconds(3), 10 * 1024 * 1024,
+                           std::chrono::hours(24));
     std::random_device rd;
     std::default_random_engine engine(rd());
     std::uniform_int_distribution<> dist(100, 1000);
@@ -67,9 +73,43 @@ void TestLogMacro()
     LOG_IF(ERROR, 2 == 1) << "conditional logging";
 }
 
+void TestWhole()
+{
+    auto file_gen = []() {
+        tm time_now;
+        time_t now = chrono::system_clock::to_time_t(chrono::system_clock::now());
+        localtime_s(&time_now, &now);
+        klog::PathChar timestamp[20];
+        wcsftime(timestamp, 20, L"%Y%m%d%H%M%S", &time_now);
+
+        DWORD pid = GetCurrentProcessId();
+
+        klog::PathString file_name(timestamp);
+        file_name.append(L"_").append(std::to_wstring(pid)).append(L"_test.log");
+
+        return file_name;
+    };
+
+    klog::LogWorker worker(file_gen, chrono::seconds(3), 10 * 1024 * 1024, chrono::hours(24));
+    klog::ConfigureLogger(&worker);
+
+    std::random_device rd;
+    std::default_random_engine engine(rd());
+    std::uniform_int_distribution<> dist(100, 1000);
+    int count = 10;
+    for (auto i = 0; i < count; ++i) {
+        std::ostringstream ss;
+        ss << "This is the " << i << "th trial";
+        LOG(INFO) << ss.str();
+        std::this_thread::sleep_for(std::chrono::milliseconds(dist(engine)));
+    }
+
+    std::cout << "-eof-" << std::endl;
+}
+
 int main()
 {
-    TestLogMacro();
+    TestWhole();
     _getch();
     return 0;
 }
